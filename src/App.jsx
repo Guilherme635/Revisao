0c3f5a345ac5d81b717efcd32d7510e5b8e134f5
Guilherme <gui.scosta01@gmail.com>
2023-08-10 13:47:14 -0300

    primeiro

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+
+    <header>
+      <h1> login</h1>
+    </header>
+
+    <main>
+      <form action=''>
+    <div>
+      <label htmlFor="usuario">Usuario</label>
+      <input type="text" id="usuario" placeholder='Digite seu usuario' />
+    </div>
+
+    <button>entrar</button>
+
+      </form>
+    </main>
+      <div>
+       
+      </div>
+      
+    </>
+  )
+}
+
+export default App
